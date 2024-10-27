ea6bcb3d7e622c19c02b7025df331ad1118c09ee
unknown <ramzynet77@gmail.com>
2024-10-28 00:43:38 +0100

    first commit

---
+++ src/main/java/com/alibou/book/config/OpenApiConfig.java
@@ -0,0 +1,42 @@
+package com.alibou.book.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.OAuthFlow;
+import io.swagger.v3.oas.annotations.security.OAuthFlows;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "jribi",
+                        email = "contact@jribi.com",
+                        url = ""
+                ),
+                description = "OpenApi documentation for Spring Security",
+                title = "OpenApi specification - Ramzi Jribi",
+                version = "1.0",
+                license = @License(
+                        name = "Licence name",
+                        url = "https://some-url.com"
+                ),
+                termsOfService = "Terms of service"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8088/api/v1"
+                )
+
+        }
+
+)
+
+public class OpenApiConfig {
+}
